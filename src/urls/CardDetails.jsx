74bc9f037890e1d7e82d9a13b8e8d066f8986eee
Krunal Unjiya <163394912+krunal2007@users.noreply.github.com>
2025-06-26 19:46:19 +0530

    Update CardDetails.jsx

--- src/urls/CardDetails.jsx
+++ src/urls/CardDetails.jsx
@@ -51,7 +51,7 @@ export default function CardDetails({ url }) {
           <div className="w-full md:w-4/6 flex items-center space-x-2">
             {!editModeEnabled && (
               <a
-                href={"http://localhost:8000/" + url.shortUrl}
+                href={"https://url-s-backend-ecru.vercel.app" + url.shortUrl}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-500 hover:underline"
@@ -78,7 +78,7 @@ export default function CardDetails({ url }) {
               <button
                 type="button"
                 onClick={() => {
-                  setClipboard(`http://localhost:8000/${url.shortUrl}`);
+                  setClipboard(`https://url-s-backend-ecru.vercel.app/${url.shortUrl}`);
                   toast("Short url copied to clipboard.", {
                     theme: "colored",
                   });
